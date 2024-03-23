9cfba6a77441a1bb8b923eeb11878754a230987a
Ari F <aryeh.frankel@yahooinc.com>
2024-03-23 00:20:13 -0400

    tox

--- src/baltimorestrings/argparse_showhelp/argparse_help_on_error.py
+++ src/baltimorestrings/argparse_showhelp/argparse_help_on_error.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 import sys
 
+
 class ArgumentParserDisplayHelpOnError(ArgumentParser):
     """No-one ever remembers that --help exists. This'll force it out whenever argparse fails
 
@@ -12,9 +13,9 @@ class ArgumentParserDisplayHelpOnError(ArgumentParser):
 
     def __init__(self, *args, **kwargs):
         self._error_line: str = self._line_length * self._line_char
-        super().__init__(formatter_class=RawDescriptionHelpFormatter, *args, **kwargs)
+        super().__init__(*args, **{"formatter_class": RawDescriptionHelpFormatter, **kwargs})
 
-    def error(self, message: str) -> str:
+    def error(self, message: str):
         sys.stderr.write(f"{self._error_line}\nError: {message}\n{self._error_line}\n")
         self.print_help()
         sys.exit(1)
